3d76774fcfdfc2f90c198e5dcf0fcc4d668f0bb6
Iaroslav <Iaroslav.aleksandrovich.beldin@gmail.com>
2023-09-29 07:19:30 +0300

    Initial commit

---
+++ FlipClock/FlipClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipClockApp.swift
+//  FlipClock
+//
+//  Created by Iaroslav Beldin on 29.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlipClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
